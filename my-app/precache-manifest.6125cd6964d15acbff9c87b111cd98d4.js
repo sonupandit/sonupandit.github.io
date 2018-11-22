344932f3969be995474ca05d6baffe7960b8f898
Sonu Pandit <pndtsonu@gmail.com>
2018-11-22 14:56:08 +0530

    Add files via upload

---
+++ my-app/precache-manifest.6125cd6964d15acbff9c87b111cd98d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "60647441b8f9857c2249",
+    "url": "/my-app/static/js/main.60647441.chunk.js"
+  },
+  {
+    "revision": "8841522072b228800ac5",
+    "url": "/my-app/static/js/1.88415220.chunk.js"
+  },
+  {
+    "revision": "60647441b8f9857c2249",
+    "url": "/my-app/static/css/main.68558160.chunk.css"
+  },
+  {
+    "revision": "8841522072b228800ac5",
+    "url": "/my-app/static/css/1.62d8a2de.chunk.css"
+  },
+  {
+    "revision": "d09ebf6125db85162316c217682a99cd",
+    "url": "/my-app/index.html"
+  }
+];
